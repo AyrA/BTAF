2e39924ae6b5eba3acafdd24735dac96acfdda8e
Kevin Gut <kevin.gut@gmail.com>
2025-09-21 18:04:23 +0200

    Add monitor to service

--- BTAF.Service/BTAFService.cs
+++ BTAF.Service/BTAFService.cs
@@ -4,17 +4,22 @@ namespace BTAF.Service
 {
     public partial class BTAFService : ServiceBase
     {
+        private readonly AudioMonitor monitor;
+
         public BTAFService()
         {
             InitializeComponent();
+            monitor = new AudioMonitor();
         }
 
         protected override void OnStart(string[] args)
         {
+            monitor.Start();
         }
 
         protected override void OnStop()
         {
+            monitor.Stop();
         }
     }
 }
